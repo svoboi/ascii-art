e56ff949300938f8104849a818fe5382cee3de42
Ivana Svobodova <svoboi11@fit.cvut.cz>
2023-11-19 17:43:14 +0100

    Main, uses all the defined things to convert example to art

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,33 @@
 package Main
 
+import exporters.ASCII.StdOutputASCIIExporter
+import transformers.ASCIIFilters.{AdjustBrightnessFilter, FlipASCIIFilter, InvertASCIIFilter}
+import transformers.{LinearNumberToSymbolTransformer, NumberASCIITransformer, NumberToSymbolASCIIArt}
+
+import java.io.File
+import javax.imageio.ImageIO
+
 object Main extends App {
-  println("Hello there")
+  val imagePath = "exampleSMALLER.png"
+  val myPicture = ImageIO.read(new File(imagePath))
+
+  val tablelist: Array[Char] = " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$".toCharArray
+  val linearTransformer: LinearNumberToSymbolTransformer = new LinearNumberToSymbolTransformer(tablelist)
+  val numberASCIITransformer: NumberASCIITransformer = new NumberASCIITransformer
+  val symbolASCIITransformer: NumberToSymbolASCIIArt = new NumberToSymbolASCIIArt(linearTransformer)
+  val invertASCIITransformer: InvertASCIIFilter = new InvertASCIIFilter
+  val brightnessFilter: AdjustBrightnessFilter = new AdjustBrightnessFilter(5)
+
+  val transformedArt = numberASCIITransformer.transform(myPicture)
+  val xFlipper: FlipASCIIFilter = new FlipASCIIFilter('x')
+  val yFlipper: FlipASCIIFilter = new FlipASCIIFilter('y')
+  val flippedArt = yFlipper.transform(transformedArt)
+  val invertedArt = invertASCIITransformer.transform(flippedArt)
+  val brighterArt = brightnessFilter.transform(invertedArt)
+
+  val finalArt = symbolASCIITransformer.transform(brighterArt)
+
+  val streamExporter = new StdOutputASCIIExporter
+  streamExporter.export(finalArt)
+
 }
