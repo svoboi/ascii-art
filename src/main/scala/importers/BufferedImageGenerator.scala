61e2c243c78617adb38b345804be61f23d6cc928
Ivana Svobodova <svoboi11@fit.cvut.cz>
2023-12-17 17:34:36 +0100

    Add picture generating logic

--- src/main/scala/importers/BufferedImageGenerator.scala
+++ src/main/scala/importers/BufferedImageGenerator.scala
@@ -4,7 +4,17 @@ import java.awt.image.BufferedImage
 
 class BufferedImageGenerator extends Importer[BufferedImage]{
   def importFunc(): BufferedImage = {
-    new BufferedImage (100, 100, BufferedImage.TYPE_INT_RGB)
-    //todo the rest of generating, and random size
+    val rand = new scala.util.Random
+    val width = rand.nextInt(100)
+    val height = rand.nextInt(100)
+    val image : BufferedImage = new BufferedImage (width.toInt, height.toInt, BufferedImage.TYPE_INT_RGB)
+    for (x <- 0 until width.toInt) {
+      for (y <- 0 until height.toInt) {
+//        val pixelValue = ((random()*10000000000L)%16777215).toInt
+        val pixelValue = rand.nextInt(16777215)
+        image.setRGB(x, y, pixelValue);
+      }
+    }
+    return image;
   }
 }
