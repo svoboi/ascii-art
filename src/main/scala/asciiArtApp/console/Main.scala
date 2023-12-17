a511d904de49e524d649b46f9878e6913b7f97a6
Ivana Svobodova <svoboi11@fit.cvut.cz>
2023-12-17 10:54:12 +0100

    Update examples in main

--- src/main/scala/asciiArtApp/console/Main.scala
+++ src/main/scala/asciiArtApp/console/Main.scala
@@ -5,16 +5,16 @@ import asciiArtApp.console.views.ConsoleView
 
 object Main extends App {
   val example1 = List("--image", "side-eye-smaller.jpg", "--table", "non-linear", "--brightness", "100", "--output-console")
-  //  example1 : run --image side-eye-smaller.jpg --table non-linear --brightness 100 --output-console
+  //  example1 : run --image "examples/side-eye-smaller.jpg" --table non-linear --brightness 100 --output-console
 
   val example2 = List("--image", "planet-smaller.png",  "--table", "bourke-small", "--output-console")
-  //  example2 : run --image planet-smaller.png --table bourke-small --output-console
+  //  example2 : run --image "examples/planet-smaller.png" --table bourke-small --output-console
 
-  //  example3 : run --image pikachu.png --flip x --table bourke-small --output-console
-  //  example4 : run --image pikachu.png --invert --table bourke-small --output-console
-  //  example5 : run --image tiger.png --table bourke-standard --output-file outcome.txt --brightness -20
-  //  example6 : run --image tiger.png --table non-linear --output-file outcome.txt --brightness -20
-  //  example6 : run --image "planet with whitespace.png" --custom-table " .:*O#@" --output-console
+  //  example3 : run --image "examples/pikachu.png" --flip x --table bourke-small --output-console
+  //  example4 : run --image "examples/pikachu.png" --invert --table bourke-small --output-console
+  //  example5 : run --image "examples/tiger.png" --table bourke-standard --output-file tiger.txt --brightness -20
+  //  example6 : run --image "examples/tiger.png" --table non-linear --output-file examples/tiger-nonlinear.txt --brightness -20
+  //  example6 : run --image "examples/planet with whitespace.png" --custom-table " .:*O#@" --output-console
 
 
   val consoleView = new ConsoleView(new ConsoleController)
