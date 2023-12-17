ecccc6d9cf95f6a13a241a2a79d3e70e863819bd
Ivana Svobodova <svoboi11@fit.cvut.cz>
2023-12-17 18:17:42 +0100

    Fix nonlinear transformer from grayscale pixel to character

--- src/main/scala/transformers/pixelToCharTransformers/HigherMiddleContrastGrayscalePixelToCharTransformer.scala
+++ src/main/scala/transformers/pixelToCharTransformers/HigherMiddleContrastGrayscalePixelToCharTransformer.scala
@@ -4,12 +4,15 @@ import transformers.NumberToCharTransformer
 
 class HigherMiddleContrastGrayscalePixelToCharTransformer extends NumberToCharTransformer{
   private val charactersTable: Array[Char] = " .:-=+*#%@".toCharArray
-  // - 3 because theres - 2 for the two characters used on sides
-  private val boxSize : Double = (200.0 - 50.0) / (charactersTable.length - 3)
+  // - 2 for the two characters used on 0-50 and 200-255
+  private val boxSize : Double = (200.0 - 50.0) / (charactersTable.length - 2)
 
   override def transform(number: Double): Char = {
+    if (number == 255.0) {
+      charactersTable.charAt(charactersTable.length - 1)
+    }
     if (number < 50) return charactersTable(0)
-    if (number < 200) return charactersTable((number / boxSize).floor.toInt)
+    if (number < 200) return charactersTable(((number-50) / boxSize).floor.toInt + 1)
     return charactersTable(9)
   }
 }
