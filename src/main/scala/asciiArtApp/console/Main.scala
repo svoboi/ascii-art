47eae3afe49abbe4b4ac40d7b38bc9ce7676bbf8
Ivana Svobodova <svoboi11@fit.cvut.cz>
2023-12-17 20:21:25 +0100

    Update main

--- src/main/scala/asciiArtApp/console/Main.scala
+++ src/main/scala/asciiArtApp/console/Main.scala
@@ -2,23 +2,20 @@ package asciiArtApp.console
 
 import asciiArtApp.console.controllers.ConsoleController
 import asciiArtApp.console.views.ConsoleView
+import exporters.text.StdOutputTextExporter
 
 object Main extends App {
-  val example1 = List("--image", "side-eye-smaller.jpg", "--table", "non-linear", "--brightness", "100", "--output-console")
   //  example1 : run --image "examples/side-eye-smaller.jpg" --table non-linear --brightness 100 --output-console
-
-  val example2 = List("--image", "planet-smaller.png",  "--table", "bourke-small", "--output-console")
   //  example2 : run --image "examples/planet-smaller.png" --table bourke-small --output-console
-
   //  example3 : run --image "examples/pikachu.png" --flip x --table bourke-small --output-console
   //  example4 : run --image "examples/pikachu.png" --invert --table bourke-small --output-console
-  //  example5 : run --image "examples/tiger.png" --table bourke-standard --output-file tiger.txt --brightness -20
+  //  example5 : run --image "examples/tiger.png" --table bourke-standard --output-file examples/tiger.txt --brightness -20
   //  example6 : run --image "examples/tiger.png" --table non-linear --output-file examples/tiger-nonlinear.txt --brightness -20
-  //  example6 : run --image "examples/planet with whitespace.png" --custom-table " .:*O#@" --output-console
+  //  example7 : run --image "examples/planet with whitespace.png" --custom-table " .:*O#@" --output-console
+  //  example8 : run --image "examples/planet-smaller.png" --custom-table " .:*O#@" --output-console
+  //  example9 : run --image-random --custom-table " .:*O#@" --output-console
 
-
-  val consoleView = new ConsoleView(new ConsoleController)
+  val consoleView = new ConsoleView(new ConsoleController, new StdOutputTextExporter)
   consoleView.run(args.toList)
-
 
 }
