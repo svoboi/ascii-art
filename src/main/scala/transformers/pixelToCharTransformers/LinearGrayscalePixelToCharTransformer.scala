b88a3d77b8a0e1cfd55b610de1e48b345556c1a3
Ivana Svobodova <svoboi11@fit.cvut.cz>
2023-12-17 17:35:04 +0100

    Fix linear transformation from number to character from table

--- src/main/scala/transformers/pixelToCharTransformers/LinearGrayscalePixelToCharTransformer.scala
+++ src/main/scala/transformers/pixelToCharTransformers/LinearGrayscalePixelToCharTransformer.scala
@@ -3,9 +3,12 @@ package transformers.pixelToCharTransformers
 import transformers.NumberToCharTransformer
 
 class LinearGrayscalePixelToCharTransformer(charactersTable: Array[Char]) extends NumberToCharTransformer {
-  private val boxSize: Double = 255.0 / (charactersTable.length - 1)
+  private val boxSize: Double = 255.0 / (charactersTable.length)
 
   override def transform(number: Double): Char = {
+    if (number == 255.0) {
+      charactersTable.charAt(charactersTable.length-1)
+    }
     charactersTable.charAt((number / boxSize).floor.toInt)
   }
 }
