aab22c670fb894a46b13c96ee71b8596d525afc7
Ivana Svobodova <svoboi11@fit.cvut.cz>
2023-12-19 10:20:46 +0100

    update transform logic of ascii image to string transformer

--- src/main/scala/transformers/ASCIIImageToStringTransformer.scala
+++ src/main/scala/transformers/ASCIIImageToStringTransformer.scala
@@ -5,11 +5,6 @@ import asciiArtApp.models.ASCIIImage
 class ASCIIImageToStringTransformer extends Transformer[ASCIIImage, String] {
 
   override def transform(image: ASCIIImage): String = {
-    var string : String = new String()
-    for(line <- image.getPixels()) {
-      string = string.concat(line.mkString)
-      string = string.concat("\n")
-    }
-    string
+    image.getPixels().map(_.mkString).mkString("\n")
   }
 }
